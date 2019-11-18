a0e156fb23746200fc38b47d6b9fa9809482cc12
Peter Ellis <peterellis.work@outlook.com>
2019-11-18 14:34:46 +0000

    WK5: Renamed package of week 5 coursework back to what it should be.

--- week5/src/main/java/com/uni/year2/week5/exercise5x13/Cylinder.java
+++ week5/src/main/java/com/uni/year2/week5/exercise5x13/Cylinder.java
@@ -7,41 +7,41 @@
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  ******************************************************************************/
 
-package com.uni.year2.week8.exercise5x12;
+package com.uni.year2.week5.exercise5x13;
 
-public class EmpericalAnalysisAlgorithm {
+public class Cylinder extends Circle {
 
-    public static void main(String... args) {
-        long start1 = System.nanoTime();
-        algo(50);
-        long end1 = System.nanoTime();
-        long time1 = end1 - start1;
+    private double height;
 
-        long start2 = System.nanoTime();
-        algo(50_000);
-        long end2 = System.nanoTime();
-        long time2 = end2 - start2;
-
-        long start3 = System.nanoTime();
-        algo(500_000);
-        long end3 = System.nanoTime();
-        long time3 = end3 - start3;
-
-        long start4 = System.nanoTime();
-        algo(500_000);
-        long end4 = System.nanoTime();
-        long time4 = end4 - start4;
-
-        System.out.println(String.format("Times of execution in nanoseconds: \n1: %s,\n2: %s,\n3: %s,\n4: %s,", time1, time2, time3, time4));
+    public Cylinder() {
+        super();
+        this.height = 1.0;
     }
 
-    public static int[] algo(final int size) {
-        int arr[] = new int[size];
-
-        for (int i = 0; i < size; i++) {
-            arr[i] = i - 1;
-        }
-
-        return arr;
+    public Cylinder(final double radius, final double height) {
+        super(radius);
+        this.height = height;
     }
+
+    public Cylinder(final double radius) {
+        this(radius, 1.0);
+    }
+
+    public Cylinder(final double radius, final double height, final String colour) {
+        super(radius, colour);
+        this.height = height;
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getVolume() {
+        return this.getArea() * this.height;
+    }
+
 }
